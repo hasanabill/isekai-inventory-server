1cbcb5b7aee80dab87c32cce42f45b6e9830f731
hasanabil <mahmudul.anabil2862@gmail.com>
2022-05-07 22:34:26 +0600

    api of all items added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // middleware
 app.use(cors());
@@ -10,5 +12,35 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('The Server is up and running')
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mstm0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('isekaiItem').collection('items');
+
+        app.get('/items', async (req, res) => {
+            const query = {}
+            const cursor = itemCollection.find(query)
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await itemCollection.findOne(query)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port)
